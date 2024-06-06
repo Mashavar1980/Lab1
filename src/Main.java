2c13c0ee13b57a9f13e9fc8a5125dea541a1387d
Mashavar1980 <zixa20152015@gmail.com>
2024-06-06 22:55:38 +0300

    Вывод цифры

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
+
 import javax.swing.*;
 
 public class Main {
+
     public static void main(String[] args) {
+        System.out.println("Hello world!");
 
-        System.out.println("Hello, World!");
-
-        JFrame f = new JFrame( "");
+        JFrame f = new JFrame("Swing Paint Demo");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         MyPanel myPanel = new MyPanel();
         myPanel.repaint();
+        myPanel.setSymbols("0");
         f.add(myPanel);
         f.pack();
         f.setVisible(true);
